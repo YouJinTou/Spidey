9975e27a9200d272ea0466a6d2a9b9447b1a3aa3
Dimitar Dimitrov <dandreevd@gmail.com>
2018-09-10 16:40:41 +0300

    Finished the first version of the ZemiBgParser. Used HtmlAgilityPack.

--- Spidey/Spidey.SDK/Land/Parsers/ZemiBgParser.cs
+++ Spidey/Spidey.SDK/Land/Parsers/ZemiBgParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 
 namespace Spidey.SDK.Land.Parsers
@@ -7,7 +8,56 @@ namespace Spidey.SDK.Land.Parsers
     {
         public IEnumerable<Ad> ParseContent(string content)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var doc = new HtmlDocument();
+
+                doc.LoadHtml(content);
+
+                var adRows = doc.DocumentNode.SelectNodes("//tr[starts-with(@id, 'aIDtr')]");
+                var ads = new List<Ad>();
+
+                foreach (var row in adRows)
+                {
+                    try
+                    {
+                        var adCells = row.SelectNodes("//td[starts-with(@id, 'aIDtd')]");
+                        var date = adCells[1].InnerText;
+                        var area = adCells[2].InnerText;
+                        var region = adCells[3].InnerText;
+                        var land = adCells[4].InnerText;
+                        var surfaceArea = adCells[5].InnerText;
+                        var category = adCells[6].InnerText;
+                        var type = adCells[7].InnerText;
+                        var price = adCells[8].InnerText;
+                        var ad = new Ad
+                        {
+                            Location = $"{area}, {region}, {land}",
+                            Area = double.TryParse(surfaceArea, out double parsedArea) ?
+                                parsedArea :
+                                (double?)null,
+                            ContactName = string.Empty,
+                            ContactPhone = string.Empty,
+                            DatePublished = date,
+                            Description = string.Empty,
+                            Price = decimal.TryParse(price, out decimal parsedPrice) ?
+                                parsedPrice :
+                                (decimal?)null
+                        };
+
+                        ads.Add(ad);
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                return ads;
+            }
+            catch
+            {
+                throw new Exception("Could not parse HTML from zemi.bg.");
+            }
         }
     }
 }
