4948c2bbcd6d4b4a37a1db08331026a1aebd1ccd
Dimitar Dimitrov <dandreevd@gmail.com>
2018-09-10 16:03:14 +0300

    Partly implemented ZemiBgCrawler.

---
+++ Spidey/Spidey.SDK/Land/Crawlers/ZemiBgCrawler.cs
@@ -0,0 +1,33 @@
+﻿using Spidey.SDK.Web;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Spidey.SDK.Land.Crawlers
+{
+    public class ZemiBgCrawler : Crawler
+    {
+        private const string Endpoint = "http://www.zemi.bg/adv-search.php?Areas={0}&Regions={1}&Lands={2}&aIDorderby=aFromDate&aIDdirection=desc&aIDpage=1";
+
+        private IEnumerable<SearchSetting> searchSettings;
+
+        public ZemiBgCrawler(IPageReader reader, IEnumerable<SearchSetting> searchSettings)
+            : base(reader)
+        {
+            this.searchSettings = searchSettings ?? 
+                throw new ArgumentException(nameof(searchSettings));
+        }
+
+        public override async Task<IEnumerable<Ad>> GetAdsAsync()
+        {
+            foreach (var setting in this.searchSettings)
+            {
+                var formattedEndpoint = string.Format(
+                    Endpoint, (int)setting.Area, (int)setting.Region, (int)setting.Land);
+                var html = await this.reader.DownloadStringAsync(formattedEndpoint);
+            }
+
+            return null;
+        }
+    }
+}
