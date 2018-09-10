30712ff4c9f37cd4c7b4d7b64293754359aed7b0
Dimitar Dimitrov <dandreevd@gmail.com>
2018-09-10 16:02:40 +0300

    Added SearchSetting.

---
+++ Spidey/Spidey.SDK/Land/SearchSetting.cs
@@ -0,0 +1,18 @@
+﻿namespace Spidey.SDK.Land
+{
+    public class SearchSetting
+    {
+        public SearchSetting(Area area, Region region, Land land)
+        {
+            this.Area = area;
+            this.Region = region;
+            this.Land = land;
+        }
+
+        public Area Area { get; set; }
+
+        public Region Region { get; set; }
+
+        public Land Land { get; set; }
+    }
+}
