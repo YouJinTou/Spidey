1f51a97144f7c22ac5693016f1da3024408c5c11
Dimitar Dimitrov <dandreevd@gmail.com>
2018-09-10 16:02:59 +0300

    Added Crawler.

---
+++ Spidey/Spidey.SDK/Land/Crawlers/Crawler.cs
@@ -0,0 +1,18 @@
+﻿using Spidey.SDK.Web;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Spidey.SDK.Land.Crawlers
+{
+    public abstract class Crawler
+    {
+        protected IPageReader reader;
+
+        public Crawler(IPageReader reader)
+        {
+            this.reader = reader;
+        }
+
+        public abstract Task<IEnumerable<Ad>> GetAdsAsync();
+    }
+}
