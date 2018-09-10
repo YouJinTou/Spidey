0f8c6b90d96879a59f80b8a690e4b7f18a09f769
Dimitar Dimitrov <dandreevd@gmail.com>
2018-09-10 16:03:55 +0300

    Began implementing ZemiBgParser.

---
+++ Spidey/Spidey.SDK/Land/Parsers/ZemiBgParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spidey.SDK.Land.Parsers
+{
+    public class ZemiBgParser : IParser
+    {
+        public IEnumerable<Ad> ParseContent(string content)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
