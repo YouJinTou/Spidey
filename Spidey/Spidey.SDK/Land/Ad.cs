07799e1748a2fca8cb0a86f28e807cfb311bc5e2
Dimitar Dimitrov <dandreevd@gmail.com>
2018-09-10 16:02:19 +0300

    Moved to Land.

---
+++ Spidey/Spidey.SDK/Land/Ad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Spidey.SDK
+{
+    public class Ad
+    {
+        public string Location { get; set; }
+
+        public double Area { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime DatePublished { get; set; }
+
+        public string ContactName { get; set; }
+
+        public string ContactPhone { get; set; }
+    }
+}
